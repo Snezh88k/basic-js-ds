a239c8b81c5a9c2dfda34973a99f694201cda5ef
Snezh88k <snezh88k@gmail.com>
2023-04-16 00:45:44 +0300

    add solutions queue

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
 }
 
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+const l = convertArrayToList([1, 2, 3, 4, 7, 3, 2]);
+console.log(l);
+
+function removeKFromList(l, k) {
+  if (l === null) {
+    return l;
+  }
+
+  let current = l;
+  let prev = null;
+
+  console.log(current, "current");
+
+  while (l) {
+    if (l.value === k && prev === null) {
+      current = l = l.next;
+      continue;
+    } else if (l.value === k && prev !== null) {
+      prev.next = l.next;
+      l = l.next;
+      continue;
+    }
+    prev = l;
+    l = l.next;
+  }
+  return current;
+}
+
+console.log(removeKFromList(l, 2));
+
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
